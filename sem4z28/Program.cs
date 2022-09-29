85086d6456b6b3094d0d775593de1f34da3da01b
evgenii <evgenii.lyabov@mail.ru>
2022-09-29 16:41:14 +0300

    add sem4z28

---
+++ sem4z28/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 28: Напишите программу, которая принимает на вход число 
+//N и M и выдаёт произведение чисел от M до N.
+//2,4 -> 24
+//1,5 -> 120
+
+Console.Clear();
+Console.WriteLine("Введите число 1:");
+int M = int.Parse(Console.ReadLine());
+Console.WriteLine("Введите число 2:");
+int N = int.Parse(Console.ReadLine());
+int A(int M)
+{
+    int result = 1;
+    for (int i = M; i <= N; i++)
+    {
+        result = result * i;
+    }
+    return result;
+}
+Console.WriteLine($"Произведение от {M} до {N} = {A(M)}");
